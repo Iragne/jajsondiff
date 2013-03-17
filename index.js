4e56c9640c0a9190e16a4678c0746c6a4f1f98c1
Iragne <jai@netcosports.com>
2013-03-18 00:21:59 +0100

    update npm

--- index.js
+++ index.js
@@ -1,1 +1,2 @@
-module.exports = require('./lib/diff_match_patch_uncompressed');
+module.exports.difftext = require('./lib/diff_match_patch_uncompressed').diff_match_patch;
+module.exports.diffjson = require('./lib/ddd');
